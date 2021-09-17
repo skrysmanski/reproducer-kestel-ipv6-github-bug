3b80292050d4170c274afabbe1e25a67cc61ac29
Sebastian Krysmanski <noreply@manski.net>
2021-09-17 09:44:03 +0200

    Simplified test

--- tests/Reproducer/ReproducerTest.cs
+++ tests/Reproducer/ReproducerTest.cs
@@ -24,7 +24,6 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using AppMotor.CliApp.CommandLine.Hosting;
-using AppMotor.Core.Exceptions;
 using AppMotor.Core.Logging;
 using AppMotor.Core.Net;
 using AppMotor.Core.Net.Http;
@@ -45,31 +44,25 @@ using Shouldly;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace AppMotor.CliApp.HttpServer.Tests
+namespace Reproducer
 {
     /// <summary>
     /// These tests test that serving from and connecting to the various combinations of <see cref="SocketListenAddresses"/>
     /// and <see cref="IPVersions"/> works.
     /// </summary>
-    public sealed class IpVersionTests : TestBase
+    public sealed class ReproducerTests : TestBase
     {
-        private static readonly Lazy<string> s_ownIPv4Address = new(() => GetLocalIpAddress(IPVersions.IPv4));
+        private static readonly Lazy<string> s_ownIPv6Address = new(GetLocalIpAddress);
 
-        private static readonly Lazy<string> s_ownIPv6Address = new(() => GetLocalIpAddress(IPVersions.IPv6));
-
-        public IpVersionTests(ITestOutputHelper testOutputHelper)
+        public ReproducerTests(ITestOutputHelper testOutputHelper)
             : base(testOutputHelper)
         {
         }
 
         [Theory]
-        [InlineData(SocketListenAddresses.Loopback, IPVersions.IPv4)]
-        [InlineData(SocketListenAddresses.Loopback, IPVersions.IPv6)]
-        [InlineData(SocketListenAddresses.Loopback, IPVersions.DualStack)]
-        [InlineData(SocketListenAddresses.Any, IPVersions.IPv4)]
-        [InlineData(SocketListenAddresses.Any, IPVersions.IPv6)]
-        [InlineData(SocketListenAddresses.Any, IPVersions.DualStack)]
-        public async Task TestConnection(SocketListenAddresses listenAddress, IPVersions ipVersion)
+        [InlineData(SocketListenAddresses.Loopback)]
+        [InlineData(SocketListenAddresses.Any)]
+        public async Task TestConnection(SocketListenAddresses listenAddress)
         {
             int testPort = ServerPortProvider.GetNextTestPort();
 
@@ -77,7 +70,7 @@ namespace AppMotor.CliApp.HttpServer.Tests
 
             var serverPort = new HttpServerPort(listenAddress, testPort)
             {
-                IPVersion = ipVersion,
+                IPVersion = IPVersions.IPv6,
             };
             var app = new HttpServerApplication(new TestHttpServerCommand(serverPort, this.TestConsole));
             Task appTask = app.RunAsync(cts.Token);
@@ -86,36 +79,10 @@ namespace AppMotor.CliApp.HttpServer.Tests
             {
                 using var httpClient = HttpClientFactory.CreateHttpClient();
 
-                switch (ipVersion)
+                await ExecuteRequest(httpClient, "[::1]", testPort);
+                if (listenAddress == SocketListenAddresses.Any)
                 {
-                    case IPVersions.IPv4:
-                        await ExecuteRequest(httpClient, "127.0.0.1", testPort);
-                        if (listenAddress == SocketListenAddresses.Any)
-                        {
-                            await ExecuteRequest(httpClient, s_ownIPv4Address.Value, testPort);
-                        }
-                        break;
-
-                    case IPVersions.IPv6:
-                        await ExecuteRequest(httpClient, "[::1]", testPort);
-                        if (listenAddress == SocketListenAddresses.Any)
-                        {
-                            await ExecuteRequest(httpClient, $"[{s_ownIPv6Address.Value}]", testPort);
-                        }
-                        break;
-
-                    case IPVersions.DualStack:
-                        await ExecuteRequest(httpClient, "127.0.0.1", testPort);
-                        await ExecuteRequest(httpClient, "[::1]", testPort);
-                        if (listenAddress == SocketListenAddresses.Any)
-                        {
-                            await ExecuteRequest(httpClient, s_ownIPv4Address.Value, testPort);
-                            await ExecuteRequest(httpClient, $"[{s_ownIPv6Address.Value}]", testPort);
-                        }
-                        break;
-
-                    default:
-                        throw new UnexpectedSwitchValueException(nameof(ipVersion), ipVersion);
+                    await ExecuteRequest(httpClient, $"[{s_ownIPv6Address.Value}]", testPort);
                 }
             }
             finally
@@ -187,59 +154,34 @@ namespace AppMotor.CliApp.HttpServer.Tests
         }
 
         [MustUseReturnValue]
-        private static string GetLocalIpAddress(IPVersions ipVersion)
+        private static string GetLocalIpAddress()
         {
-            if (ipVersion == IPVersions.IPv4)
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
             {
-                // Code via: https://stackoverflow.com/a/27376368/614177
-                //
-                // NOTE: This code also works with IPv6 but there's no IPv6 network in
-                //   GitHub actions. So we'll have to use a different approach.
-                //   See: https://github.com/actions/virtual-environments/issues/668
-
-                using Socket socket = new(AddressFamily.InterNetwork, SocketType.Dgram, 0);
-
-                socket.Connect("8.8.8.8", 65530);
-
-                IPEndPoint endPoint = (IPEndPoint)socket.LocalEndPoint!;
-
-                IPAddress.IsLoopback(endPoint.Address).ShouldBe(false);
-
-                return endPoint.Address.ToString();
-            }
-            else if (ipVersion == IPVersions.IPv6)
-            {
-                foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+                if (networkInterface.OperationalStatus != OperationalStatus.Up || !networkInterface.Supports(NetworkInterfaceComponent.IPv6))
                 {
-                    if (networkInterface.OperationalStatus != OperationalStatus.Up || !networkInterface.Supports(NetworkInterfaceComponent.IPv6))
-                    {
-                        continue;
-                    }
-
-                    var ipProperties = networkInterface.GetIPProperties();
-
-                    if (ipProperties.GatewayAddresses.Count == 0)
-                    {
-                        continue;
-                    }
-
-                    foreach (var ip in ipProperties.UnicastAddresses)
-                    {
-                        if (ip.Address.AddressFamily != AddressFamily.InterNetworkV6 || IPAddress.IsLoopback(ip.Address))
-                        {
-                            continue;
-                        }
-
-                        return ip.Address.ToString();
-                    }
+                    continue;
                 }
 
-                throw new InvalidOperationException("No usable IPv6 network interface exists.");
+                var ipProperties = networkInterface.GetIPProperties();
+
+                if (ipProperties.GatewayAddresses.Count == 0)
+                {
+                    continue;
+                }
+
+                foreach (var ip in ipProperties.UnicastAddresses)
+                {
+                    if (ip.Address.AddressFamily != AddressFamily.InterNetworkV6 || IPAddress.IsLoopback(ip.Address))
+                    {
+                        continue;
+                    }
+
+                    return ip.Address.ToString();
+                }
             }
-            else
-            {
-                throw new NotSupportedException($"Unsupported ipVersion: {ipVersion}");
-            }
+
+            throw new InvalidOperationException("No usable IPv6 network interface exists.");
         }
 
         private sealed class TestHttpServerCommand : HttpServerCommandBase
